7dc65573ae932c9816844159921693f9feb8aca9
Gavin_Chen <77559123+CrypticT1tan@users.noreply.github.com>
2026-03-27 12:01:15 -0500

    Delete code_files directory

--- code_files/main.py
+++
@@ -1,8 +0,0 @@
-# Local Application Imports
-from color_palette import ColorPalette
-
-
-if __name__ == "__main__":
-    color_palette = ColorPalette()
-    # Prevent the window from closing immediately upon reaching the end of this file
-    color_palette.window.mainloop()
